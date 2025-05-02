27fd18375f85477f2cd3b3a033b4a9b0e4027603
Mohammad Zaid Shaikh <zaidshaikhhulk@gmail.com>
2025-05-02 15:10:08 +0530

    feat: update authentication middleware and add sign-in page

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,24 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)',"/",'/sign-up(.*) '])
+
+export default clerkMiddleware(async (auth, req) => {
+  const user=  auth()
+  const userId=(await user).userId
+  const url=new URL(req.url)
+
+if(userId && isPublicRoute(req) && url.pathname!=="/") {
+    return NextResponse.redirect(new URL('/dashboard', req.url))
+}
+
+
+ 
+
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [
@@ -9,4 +27,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
